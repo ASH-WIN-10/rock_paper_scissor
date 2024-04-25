a87293a4d9899e219f5b1b375c4249d24eb6e0f2
Ashvin <ashvinjangid10@gmail.com>
2024-04-25 15:55:09 +0530

    game logic complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+function getComputerSelection() {
     let choices = ["rock", "paper", "scissor"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
@@ -7,58 +7,57 @@ function playRound(playerSelection, computerSelection){
     if ((playerSelection === "rock" && computerSelection === "paper")||
         (playerSelection === "paper" && computerSelection === "scissor")||
         (playerSelection === "scissor" && computerSelection === "rock")
-    ){
-        return `You lose! ${computerSelection} beats ${playerSelection}.`;
-    }
-    else if (playerSelection === computerSelection){
-        return `It's a tie.`;
-    }
-    else {
-        return `You win! ${playerSelection} beats ${computerSelection}.`;
-    }
+    )
+        return computerScore++;
+
+    else if (playerSelection === computerSelection)
+        return;
+
+    else if ((computerSelection === "rock" && playerSelection === "paper")||
+            (computerSelection === "paper" && playerSelection === "scissor")||
+            (computerSelection === "scissor" && playerSelection === "rock")
+    )
+        return playerScore++;
 }
 
-function playGame(){
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock Paper Scissor \n(Type 'exit' to quit game.) \nEnter your choice: ").toLowerCase();
-        let computerSelection = getComputerChoice();
 
-        if (playerSelection === "exit"){
-            console.log("Thank You!");
-            break;
-        }
-        while ((playerSelection !== "rock") && (playerSelection !== "paper") && (playerSelection !== "scissor")){
-            playerSelection = prompt("Not a valid choice!\nTry again!");
-        }
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
+const buttons = document.querySelectorAll('div>button');
+
+const resultDiv = document.querySelector('#result');
+let result = document.createElement('span');
+resultDiv.appendChild(result);
+
+const playerScoreDiv = document.querySelector('#playerScore');
+let playerScoreText = document.createElement('span');
+playerScoreDiv.appendChild(playerScoreText);
+
+const computerScoreDiv = document.querySelector('#computerScore');
+let computerScoreText = document.createElement('span');
+computerScoreDiv.appendChild(computerScoreText);
+
+
+buttons.forEach(selection => {
+    selection.addEventListener('click', (e) => {
+        playerSelection = e.target.textContent.toLowerCase();
         
-        if (result.includes("win")){
-            playerScore++;
+        playRound(playerSelection, getComputerSelection());
+        
+        playerScoreText.textContent = playerScore;
+        computerScoreText.textContent = computerScore;
+       
+        if ((computerScore === 5) || (playerScore === 5)){
+            if (playerScore > computerScore)
+                result.textContent = `You Win!!`;
+    
+            else if (playerScore < computerScore)
+                result.textContent = `You Lose!!`;
+    
+            else
+                result.textContent = `It's a Tie!!`;
         }
-        else if (result.includes("lose")){
-            computerScore++;
-        }
-        else {
-            playerScore++;
-            computerScore++;
-        }
-    }
-
-    if (playerScore > computerScore){
-        return `Your score was ${playerScore} and Computer's score was ${computerScore}. \nYou Win!!`;
-    }
-    else if (playerScore < computerScore){
-        return `Your score was ${playerScore} and Computer's score was ${computerScore}. \nYou Lose!!`;
-    }
-    else {
-        return `Your score was ${playerScore} and Computer's score was ${computerScore}. \nIt's a Tie!!`;
-    }
-}
-
-
-console.log(playGame());
+    });
+});
